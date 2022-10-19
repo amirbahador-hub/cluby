f299029bac658259e12e3e5d027fe57df152e372
amirbahador <amirbahador.pv@gmail.com>
2022-10-19 14:22:44 -0400

    :sparkles: Add inactivity period benefits endpoint

--- core/selectors.py
+++ core/selectors.py
@@ -2,7 +2,7 @@ from django.db.models import QuerySet, Count, Q, Prefetch
 from datetime import datetime
 
 from core.models import Benefit, Venue, BenefitUsage
-from core.utils import get_n_days_ago, expire_benefit_lock
+from core.utils import get_n_days_ago, transition_table
 from django.utils import timezone
 
 
@@ -13,11 +13,33 @@ def popular_benefits(*, since: int = 180) -> QuerySet[Venue]:
             queryset=Benefit.objects.annotate(
                 usageCount=Count(
                     "benefitusage",
-                    filter=Q(benefitusage__usagetimestamp__lt=get_n_days_ago(since)),
+                    filter=Q(benefitusage__usagetimestamp__gt=get_n_days_ago(since)),
                 ),
             ).order_by("-usageCount"),
         )
     ).all()
+
+
+def benefits_inactivity_periods(*, venue_id: int, since: int = 180):
+
+    benefits = (
+        Benefit.objects.filter(venueid=venue_id)
+        .prefetch_related(
+            Prefetch(
+                "benefitusage_set",
+                queryset=BenefitUsage.objects.filter(
+                    usagetimestamp__gt=get_n_days_ago(since)
+                ).order_by("-usagetimestamp"),
+            )
+        )
+        .all()
+    )
+    result = list()
+    for benefit in benefits:
+        _, checker, days = transition_table[benefit.recurrence][1]
+        inactive_periods = checker(duration=days, usage=benefit.benefitusage_set.all())
+        result.append({"benefitId": benefit.id, "inactivityPeriods": inactive_periods})
+    return result
 
 
 def available_benefits(
@@ -42,11 +64,9 @@ def available_benefits(
         benefitusages = benefit.benefitusage_set.all()
         if benefitusages.count():
             latest_usage = benefitusages[0].usagetimestamp
-            if timestamp.astimezone(timezone.utc).replace(
-                tzinfo=None
-            ) < expire_benefit_lock(
-                recurrence=benefit.recurrence, timestamp=latest_usage
-            ):
+            _, checker, days = transition_table[benefit.recurrence][0]
+            expire_time = checker(additional_days=days, timestamp=latest_usage)
+            if timestamp.astimezone(timezone.utc).replace(tzinfo=None) < expire_time:
                 continue
         valid_benefits.append(benefit.id)
     return Benefit.objects.filter(id__in=valid_benefits)
--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,14 @@
 from django.utils import timezone
+from django.db.models import QuerySet, Count, Q, Prefetch
 from datetime import timedelta, datetime
 from django.utils import timezone
-from enum import Enum
+from enum import Enum, auto
+from core.models import BenefitUsage
+
+
+class Action(Enum):
+    AVAILABILITY = auto()
+    INACTIVITY = auto()
 
 
 class Recurrence(Enum):
@@ -10,17 +17,45 @@ class Recurrence(Enum):
     MONTH = "month"
 
 
-def get_n_days_ago(n: int = 180):
-    today = timezone.now().date()
-    return today - timedelta(days=n)
+def inactivity_periods(duration: int, usage: QuerySet[BenefitUsage]):
+    inactive_time = list()
+    pointer_time = datetime.now()
+    for bu in usage:
+        usage_timestamp = bu.usagetimestamp
+        if usage_timestamp < pointer_time - timedelta(days=duration):
+            inactive_time.append(
+                {
+                    "startTime": pointer_time.isoformat(" "),
+                    "pointer_time": usage_timestamp.isoformat(" "),
+                }
+            )
+        pointer_time = bu.usagetimestamp
+
+    return inactive_time
 
 
 def expire_benefit_lock(
-    recurrence: Recurrence, timestamp: datetime = datetime.now()
+    additional_days: int, timestamp: datetime = datetime.now()
 ) -> datetime:
-    if recurrence == Recurrence.DAY.value:
-        return timestamp + timedelta(days=1)
-    elif recurrence == Recurrence.WEEK.value:
-        return timestamp + timedelta(days=7)
-    elif recurrence == Recurrence.MONTH.value:
-        return timestamp + timedelta(days=30)
+    return timestamp + timedelta(days=additional_days)
+
+
+transition_table = {
+    Recurrence.DAY.value: [
+        (Action.AVAILABILITY, expire_benefit_lock, 1),
+        (Action.INACTIVITY, inactivity_periods, 14),
+    ],
+    Recurrence.WEEK.value: [
+        (Action.AVAILABILITY, expire_benefit_lock, 7),
+        (Action.INACTIVITY, inactivity_periods, 21),
+    ],
+    Recurrence.MONTH.value: [
+        (Action.AVAILABILITY, expire_benefit_lock, 30),
+        (Action.INACTIVITY, inactivity_periods, 30),
+    ],
+}
+
+
+def get_n_days_ago(n: int = 180):
+    today = timezone.now().date()
+    return today - timedelta(days=n)
