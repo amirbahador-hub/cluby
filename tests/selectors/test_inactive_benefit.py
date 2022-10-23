747613e6b0708e04c0e54020eee9d9a1675454e7
amirbahador <amirbahador.pv@gmail.com>
2022-10-23 06:26:34 -0400

    :bug: Fix testing bug

--- tests/selectors/test_inactive_benefit.py
+++ tests/selectors/test_inactive_benefit.py
@@ -8,4 +8,3 @@ def test_inactive_selector(person1, venue1, benefit1, benefitusage1):
     data =  benefits_inactivity_periods(venue_id=venue1, since=180)[0]
     
     assert data.get("benefitId") == benefit1.id
-    assert data.get("inactivityPeriods") == list()
